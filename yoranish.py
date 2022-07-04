ffc445ff25644de35288bd0523e33341c8a1da33
matej-veselovsky <matej.veselovsky97@gmail.com>
2022-07-04 22:51:18 +0200

    First database update from normaliser

--- yoranish.py
+++ yoranish.py
@@ -5,9 +5,9 @@ def translate(word, direction):
         cur = con.cursor()
 
         if direction == 0:
-            query = "SELECT czech_word FROM czech_words WHERE reference_id IN (SELECT yoranish_id FROM yoranish_words WHERE yoranish_word IS (?) COLLATE NOCASE);"
+            query = "SELECT czech_word FROM czech_words WHERE reference_id IN (SELECT yorani_id FROM yorani_words WHERE yorani_word IS (?) COLLATE NOCASE);"
         else:
-            query = "SELECT yoranish_word FROM yoranish_words WHERE yoranish_id IN (SELECT reference_id FROM czech_words WHERE czech_word IS (?) COLLATE NOCASE);"
+            query = "SELECT yorani_word FROM yorani_words WHERE yorani_id IN (SELECT reference_id FROM czech_words WHERE czech_word IS (?) COLLATE NOCASE);"
             
         cur.execute(query, [word])
         answer = cur.fetchall()
